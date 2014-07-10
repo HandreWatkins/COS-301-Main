420f75fb8f6f9ab44cb7fc65e7956b56c9853dbf
Juandre Barnard <xciverb@gmail.com>
2014-07-10 15:15:35 +0200

    quick attributes

--- Draft/dbMonitor.sql
+++ Draft/dbMonitor.sql
@@ -22,53 +22,53 @@ DROP TABLE request_Table;
 
 CREATE TABLE request_Table
 (
-  request_id integer NOT NULL DEFAULT nextval('request_id_seq'::regclass),
-  store_id smallint NOT NULL,
-  first_name character varying(45) NOT NULL,
-  last_name character varying(45) NOT NULL,
-  email character varying(50),
-  address_id smallint NOT NULL,
+  request_Date integer NOT NULL DEFAULT nextval('request_id_seq'::regclass),
+  request_Starttime smallint NOT NULL,
+  request_endtime character varying(45) NOT NULL,
+  overhead character varying(45) NOT NULL,
+  failure rate character varying(50),
+  httpStatus character varying(50) NOT NULL,
   activebool boolean NOT NULL DEFAULT true,
   create_date date NOT NULL DEFAULT ('now'::text)::date,
   last_update timestamp without time zone DEFAULT now(),
   active integer,
   CONSTRAINT customer_pkey PRIMARY KEY (customer_id),
-  CONSTRAINT customer_address_id_fkey FOREIGN KEY (address_id)
-      REFERENCES address (address_id) MATCH SIMPLE
+ -- CONSTRAINT customer_address_id_fkey FOREIGN KEY (address_id)
+    --  REFERENCES address (address_id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE RESTRICT
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE customer
+ALTER TABLE request_Table
   OWNER TO postgres;
 
 -- Index: idx_fk_address_id
 
 -- DROP INDEX idx_fk_address_id;
 
-CREATE INDEX idx_fk_address_id
-  ON customer
-  USING btree
-  (address_id);
+--CREATE INDEX idx_fk_address_id
+ -- ON customer
+ -- USING btree
+  --(address_id);
 
 -- Index: idx_fk_store_id
 
 -- DROP INDEX idx_fk_store_id;
 
-CREATE INDEX idx_fk_store_id
-  ON customer
-  USING btree
-  (store_id);
+--CREATE INDEX idx_fk_store_id
+ -- ON customer
+ -- USING btree
+--  (store_id);
 
 -- Index: idx_last_name
 
 -- DROP INDEX idx_last_name;
 
-CREATE INDEX idx_last_name
-  ON customer
-  USING btree
-  (last_name COLLATE pg_catalog."default");
+--CREATE INDEX idx_last_name
+ -- ON customer
+ -- USING btree
+ -- (last_name COLLATE pg_catalog."default");
 
 
 -- Trigger: last_updated on customer
@@ -77,7 +77,7 @@ CREATE INDEX idx_last_name
 
 CREATE TRIGGER last_updated
   BEFORE UPDATE
-  ON customer
-  FOR EACH ROW
+  ON request_Table
+FOR EACH ROW
   EXECUTE PROCEDURE last_updated();
 
