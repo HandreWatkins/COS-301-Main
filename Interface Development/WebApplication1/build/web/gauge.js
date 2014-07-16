e33fc42163f2da611d1166ffb77a4d749fe5432a
PieterLeRoux <pietlr@gmail.com>
2014-07-16 09:27:23 +0200

    Updated Intrerface
    Updated Intrerface

--- Interface Development/WebApplication1/build/web/gauge.js
+++
@@ -1,7 +0,0 @@
-        $('.value').html($('input').val());
-
-                $('input').change(function(){
-            var cur = $('input').val();
-            $(this).attr('value', cur );
-            $('.value').html(cur);
-        });
