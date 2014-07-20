eacddaf5168ec5ecd3baacb320ac99ea055eeca8
HandreWatkins <kuria.okami@gmail.com>
2014-07-20 23:58:22 +0200

    Response

--- Server_Reverceproxy/j2eeReverceproxy/src/main/java/HTTPclient/responseCall.java
+++ Server_Reverceproxy/j2eeReverceproxy/src/main/java/HTTPclient/responseCall.java
@@ -1,6 +1,9 @@
 package HTTPclient;
 
-import org.apache.http.*;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
 public class responseCall
 {
 	public responseCall() 
@@ -8,11 +11,15 @@ public class responseCall
 		// TODO Auto-generated constructor stub
 	}
 	
-	public Boolean clientreqeust()
+	public Boolean clientreqeust(HttpRequest request)
 	{
 		try 
 		{
-			DefaultHTTpClient  internal_request = new D
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			HttpResponse response = httpclient.execute((HttpUriRequest) request);
+			
+			
+			
 		} catch (Exception e)
 		{
 			// TODO: handle exception
