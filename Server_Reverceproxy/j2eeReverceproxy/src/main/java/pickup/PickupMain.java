6f30330b5680bc5d991efd823771238920c97405
HandreWatkins <kuria.okami@gmail.com>
2014-08-22 00:25:57 +0200

    File system restored

--- Server_Reverceproxy/j2eeReverceproxy/src/main/java/pickup/PickupMain.java
+++ Server_Reverceproxy/j2eeReverceproxy/src/main/java/pickup/PickupMain.java
@@ -1,11 +1,10 @@
 package pickup;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
 import javax.servlet.http.HttpServletRequest;
+
 import HTTPclient.Pushclient;
 import Util.Translate;
 import Util.UrlLinker;
@@ -15,25 +14,16 @@ public class PickupMain
 	//=================================================================================
 	@SuppressWarnings("unused")
 	private final Pushclient callClient = new Pushclient();
-	private static final ArrayList<Runnable> requestpickup = new ArrayList<Runnable>();
 	@SuppressWarnings("unused")
 	private final Translate translate = new Translate();
-	private ExecutorService executor = null;
-	private Boolean singleton = false;
 	private final String [] jax_RS = {"GET","POST","PUT"};
 	//=================================================================================
 	
-	public PickupMain(Translate trance) throws IOException
-	{	
-            executor = Executors.newFixedThreadPool(1000);
-	}
+	public PickupMain(Translate trance) throws IOException{}
 	
 	public void pickupListener(UrlLinker file) throws IOException
 	{
-            UrlLinker requestcontrol = file;//translate.linkurl(requestHTTP,responseHTTP);
-
-            Runnable connect = new PickupHandle(requestcontrol);
-            requestpickup.add(connect);
+            //UrlLinker requestcontrol = file;//translate.linkurl(requestHTTP,responseHTTP);
 	}
 	
 	//Test if httpRequest is JAX-RS or JAX-WS
@@ -43,30 +33,5 @@ public class PickupMain
 			return true;
 		else
 			return false;
-	}
-	
-	public void start()
-	{
-            singleton = true;
-
-            new Thread(new Runnable() 
-            {
-                public void run()
-                {
-                    while(singleton)
-                    {
-                        if(!requestpickup.isEmpty())
-                        {
-                            Runnable connect = requestpickup.remove(0);
-                            executor.execute(connect);
-                        }
-                    }
-                }
-            }).start();
-	}
-	
-	public boolean hasStart()
-	{
-		return singleton;
 	}
 }
