1594e664daa0d9a164f3a6e6cbe52c2167f4450a
Juandre Barnard <xciverb@gmail.com>
2014-10-11 10:09:02 +0200

    updated version

---
+++ Database/test updated/target/generated-sources/annotations/entity/Users_.java
@@ -0,0 +1,15 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.6.0.v20140809-rNA", date="2014-10-10T23:53:54")
+@StaticMetamodel(Users.class)
+public class Users_ { 
+
+    public static volatile SingularAttribute<Users, String> password;
+    public static volatile SingularAttribute<Users, Integer> userId;
+    public static volatile SingularAttribute<Users, String> username;
+
+}
