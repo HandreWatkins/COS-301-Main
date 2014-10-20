689e1e0a705387a6273c3ebe081776fd1e8ba89d
PieterLeRoux <pietlr@gmail.com>
2014-10-20 02:18:19 +0200

    FINALE PRODUK

--- SMART INTEGRATION MONITOR/interfaceWithmaven/target/generated-sources/annotations/entity/Users_.java
+++ SMART INTEGRATION MONITOR/interfaceWithmaven/target/generated-sources/annotations/entity/Users_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.0.v20140809-rNA", date="2014-10-19T21:38:31")
+@Generated(value="EclipseLink-2.6.0.v20140809-rNA", date="2014-10-20T02:15:17")
 @StaticMetamodel(Users.class)
 public class Users_ { 
 
